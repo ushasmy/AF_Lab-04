815e4f8bd503db2c808cf9193e94a6561261869b
Ummu Salma M. H. <ummusalmahasmy@gmail.com>
2026-02-23 22:22:21 +0530

    Added basic web server example

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello, Node!");
+const http = require('http');
+
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
